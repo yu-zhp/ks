da5b95c517bfdc09260e0f29c59bd89ad6ae9dc2
yzp <827122717@qq.com>
2019-08-16 16:27:57 +0800

    log module change

---
+++ ks-support/ks-log/src/main/java/org/ks/kslog/KsLogApplication.java
@@ -0,0 +1,13 @@
+package org.ks.kslog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KsLogApplication.class, args);
+    }
+
+}
